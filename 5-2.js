87caf9b9f6beeb3e4a92c5b5208b2f74509720fb
samuel <samuel@familjenlarsson.eu>
2021-12-06 14:49:33 +0100

    day 5

---
+++ 5-2.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', 'UTF-8', (error, data) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+	// Code
+	//
+	const split = data.slice(0, -1).split('\n');
+	const matrix = [];
+	for (let i = 0; i < 1000; i++) {
+		// verified visually that no coordinate exceeds 1000
+		matrix[i] = Array(1000).fill(0);
+	}
+
+	split.forEach((line) => {
+		const [one, two] = line.split(' -> ');
+		const [x1, y1] = one.split(',').map((v) => Number(v));
+		const [x2, y2] = two.split(',').map((v) => Number(v));
+
+		if (x1 === x2) {
+			// horizontal line
+			for (let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++) matrix[x1][i]++;
+		} else if (y1 === y2) {
+			// vertical line
+			for (let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) matrix[i][y1]++;
+		} else {
+			// diagonal line
+			let h = x1;
+			let v = y1;
+			const modX = x1 < x2 ? 1 : -1;
+			const modY = y1 < y2 ? 1 : -1;
+			while (h !== x2) {
+				matrix[h][v]++;
+				h += modX;
+				v += modY;
+			}
+			matrix[x2][y2]++;
+		}
+	});
+
+	let sumCross = 0;
+	matrix.forEach((column) => {
+		column.forEach((value) => {
+			if (value > 1) sumCross++;
+		});
+	});
+
+	console.log(sumCross);
+});
--- README.md
+++ README.md
@@ -16,9 +16,9 @@ My Advent of Code 2021 using JavaScript with Node.js.
 - [x] Day 4
   - [x] Task 1
   - [x] Task 2
-- [ ] Day 5
-  - [ ] Task 1
-  - [ ] Task 2
+- [x] Day 5
+  - [x] Task 1
+  - [x] Task 2
 - [ ] Day 6 (Bday)
   - [ ] Task 1
   - [ ] Task 2
